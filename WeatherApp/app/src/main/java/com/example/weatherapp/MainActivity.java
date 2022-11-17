02c9248aec6ee5e00e0758ec09a06aeacac53064
Anushcka <anushckajoshi@gmail.com>
2022-11-17 17:49:30 +0530

    ImageRequest HTTP Request to Open Weather Map to load icon for different weather codes

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.weatherapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -13,16 +13,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,7 +33,7 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -101,8 +102,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 double lon = location.getLongitude();
                 String Latitude = String.valueOf(lat);
                 String Longitude = String.valueOf(lon);
-//                Toast.makeText(MainActivity.this, Latitude, Toast.LENGTH_SHORT).show();
-//                Toast.makeText(MainActivity.this, Longitude, Toast.LENGTH_SHORT).show();
                 GeoPoint startPoint = new GeoPoint(lat, lon);
                 mapController.setCenter(startPoint);
                 Marker live_marker = new Marker(map);
@@ -117,15 +116,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void city_name(Context ctx) throws IOException {
         EditText city_input = (EditText) findViewById(R.id.city_input);
         String city = city_input.getText().toString();
+        city_input.setText("");
         Toast.makeText(MainActivity.this, city, Toast.LENGTH_SHORT).show();
         if (city.equals(" ")) {
             Toast.makeText(MainActivity.this, "EMPTY CITY", Toast.LENGTH_SHORT).show();
         }
         weatherhttp(city);
-//        geocoding(city);
 
     }
-
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -145,36 +143,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
 
         JsonObjectRequest jsonObjectRequest	= new JsonObjectRequest(url,new Response.Listener<JSONObject>() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(MainActivity.this, "Response: " + response, Toast.LENGTH_LONG).show();
+//                        Toast.makeText(MainActivity.this, "Response: " + response, Toast.LENGTH_LONG).show();
                         double double_long = 0;
                         double double_lat = 0;
+                        double temp = 0;
+                        String description = "";
+                        int humidity = 0;
+                        String icon_code="";
+                        String wind_speed = "";
                         try {
                             JSONObject coordobj = response.getJSONObject("coord");
                             String lat = coordobj.getString("lat");
                             String lon = coordobj.getString("lon");
                             double_lat = Double.parseDouble(lat);
                             double_long = Double.parseDouble(lon);
-                            Toast.makeText(MainActivity.this, "Lat: " + double_lat + "long: " + double_long, Toast.LENGTH_LONG).show();
                             String map3 = String.valueOf(map);
-                            Toast.makeText(MainActivity.this, "RESULT" + map3, Toast.LENGTH_SHORT).show();
-                            JSONArray jsonArray = response.getJSONArray("Weather");
+//                            Toast.makeText(MainActivity.this, "RESULT" + map3, Toast.LENGTH_SHORT).show();
+                            JSONArray jsonArray = response.getJSONArray("weather");
                             JSONObject weatherobj = jsonArray.getJSONObject(0);
-                            String description = weatherobj.getString("description");
+                            description = weatherobj.getString("description");
+                            icon_code = weatherobj.getString("icon");
                             JSONObject mainobj = response.getJSONObject("main");
-                            double temp = mainobj.getDouble("temp") - 273.15;
-                            int humidity = mainobj.getInt("humidity");
+                            temp = Math.round(mainobj.getDouble("temp")-273.15);
+                            humidity = mainobj.getInt("humidity");
                             JSONObject windobj = response.getJSONObject("wind");
                             JSONObject cloudobj = response.getJSONObject("clouds");
                             String clouds = cloudobj.getString("all");
-                            String wind = windobj.getString("speed");
+                            wind_speed = windobj.getString("speed");
                             JSONObject sysobj = response.getJSONObject("sys");
                             String Country = response.getString("country");
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                         map_recenter(double_lat, double_long);
+                        TextView city_text = (TextView) findViewById(R.id.City);
+                        city_text.setText(city);
+                        TextView temp_text = (TextView) findViewById(R.id.Temp);
+                        temp_text.setText(String.valueOf(temp+"\u00B0")+"c");
+                        TextView description_text = (TextView) findViewById(R.id.Description);
+                        description_text.setText(description);
+                        TextView humidity_text = (TextView) findViewById(R.id.Humidity);
+                        humidity_text.setText(humidity+"%");
+                        get_icon(icon_code);
+                        TextView wind_speed_text = (TextView) findViewById(R.id.Wind);
+                        wind_speed_text.setText(wind_speed+"m/s");
+                        Toast.makeText(MainActivity.this,wind_speed+"WINDDD", Toast.LENGTH_SHORT).show();
+
                     }
                 },
                 new Response.ErrorListener() {
@@ -189,6 +208,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 public void map_recenter(double lat, double lon) {
     map.getController().animateTo(new GeoPoint(lat, lon));
+}
+public void get_icon(String icon_code) {
+    Toast.makeText(getApplicationContext(),icon_code, Toast.LENGTH_SHORT).show();
+    RequestQueue requestqueue = Volley.newRequestQueue(getApplicationContext());
+    String icon_owp_url = "https://openweathermap.org/img/wn/"+icon_code+"@2x.png";
+    int max_width=300;
+    int max_height=300;
+    ImageRequest imageRequest = new ImageRequest(icon_owp_url, new Response.Listener<Bitmap>() {
+        @Override
+        public void onResponse(Bitmap response) {
+            ImageView imageView = (ImageView) findViewById(R.id.icon);
+            imageView.setImageBitmap(response);
+        }
+    },max_width, max_height, null,
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error)
+                {Log.d("image_Error", String.valueOf(error));
+                    Toast.makeText(getApplicationContext(), "Error: "+error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+    requestqueue.add(imageRequest);
 
 }
 }
